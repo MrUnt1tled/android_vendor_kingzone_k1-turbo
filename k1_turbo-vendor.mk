bd7ae07124de993be8496c1baa9fadbbc1e9f828
John Reinel Caluag <johnrenielcaluag1009@gmail.com>
2016-05-30 10:58:00 +0800

    Update to x300

--- k1_turbo-vendor.mk
+++ k1_turbo-vendor.mk
@@ -1,9 +1,9 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/proprietary/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cherrymobile/x300/proprietary/vendor,system/vendor)
